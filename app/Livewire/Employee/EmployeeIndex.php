5719647440a29cce06f7d6baed62525b7bc5f78b
André <andrevera.pe@gmail.com>
2025-10-09 23:37:27 -0500

    feat: delete added

--- app/Livewire/Employee/EmployeeIndex.php
+++ app/Livewire/Employee/EmployeeIndex.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Employee;
 use App\Models\Employee;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\Log;
 
 class EmployeeIndex extends Component
 {
@@ -14,6 +15,8 @@ class EmployeeIndex extends Component
 
     protected $queryString = ['search'];
 
+    public $employeeIdToDelete;
+
     public function render()
     {
         $employees = Employee::paginate(10);
@@ -21,5 +24,29 @@ class EmployeeIndex extends Component
         return view('livewire.employee.employee-index', [
             'employees' => $employees
         ]);
+    }
+
+    public function setEmployeeToDelete($employeeId)
+    {
+        $this->employeeIdToDelete = $employeeId;        
+         Log::info('store:', [
+                'valor' => $this->employeeIdToDelete
+            ]);     
+        $this->dispatch('openDeleteModal');
+    }
+
+    public function delete()
+    {
+        try {
+            $employee = Employee::findOrFail($this->employeeIdToDelete);
+            $employee->delete();
+
+            $this->employeeIdToDelete = null; // Reset after deletion
+            $this->dispatch('closeDeleteModal'); // Emite evento para Alpine            
+            session()->flash('success', 'El trabajador fue eliminado correctamente.');
+        } catch (\Throwable $th) {
+            Log::error('Error deleting employee: ' . $th->getMessage());
+            session()->flash('error', 'No se pudo eliminar el trabajador.');
+        }
     }
 }
